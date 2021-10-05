6c5de2c30645cfe8eca02c77db6111cbfbf8add0
Oksana Churkina <ochurkina@parseq.pro>
2021-10-05 23:30:09 +0300

    Fix

--- src/com/company/MainClass.java
+++ src/com/company/MainClass.java
@@ -1,34 +1,38 @@
 package com.company;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MainClass {
 
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> newList = new ArrayList<>();
-        for (int i : intList) {
+        int[] arr = new int[]{1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4};
+        int size = 0;
+        for(int i : arr) {
             if ((i > 0) && (i % 2 == 0)) {
-                newList.add(i);
+                size++;
             }
         }
-        bubbleSort(newList);
-        System.out.println(newList);
+        int[] newArr = new int[size];
+        size = 0;
+        for(int i : arr){
+            if ((i > 0) && (i % 2 == 0)) {
+                newArr[size++] = i;
+            }
+        }
+        bubbleSort(newArr);
+        for(int i : newArr){
+            System.out.println(i);
+        }
     }
 
-
-    static void bubbleSort(List<Integer> list) {
-        int n = list.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (list.get(j) > list.get(j + 1)) {
-                    int temp = list.get(j);
-                    list.set(j, list.get(j + 1));
-                    list.set(j + 1, temp);
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
-            }
-        }
     }
 }
